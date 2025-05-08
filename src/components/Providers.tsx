fbfa26ee6d67390711170ee9f083aac2330dbf1f
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2025-05-08 19:07:16 +0000

    wip

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,15 +1,12 @@
 "use client";
 
-import { ContentProvider } from "@/lib/content-context";
 import { Toaster } from "react-hot-toast";
-import { GlobalErrorHandler } from "@/utils/global-error-handler";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ContentProvider>
+    <>
       {children}
-      <GlobalErrorHandler />
       <Toaster position="bottom-right" />
-    </ContentProvider>
+    </>
   );
 } 
